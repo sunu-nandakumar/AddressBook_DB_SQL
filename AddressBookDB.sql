09c534d4de78bf8bc9602cc20c951396fddc0f79
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-07-25 21:56:25 +0530

    adding the count of the table using state or city

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -49,5 +49,19 @@ FROM
     address_book
 WHERE
     city = 'vapi';
+#uc7
+#Ability to understand the size of address book by City and State
+
+#size of address book by city
+SELECT 
+    COUNT(city) AS NumberOfaddress_book
+FROM
+    address_book;
+
+#size of address book by state
+SELECT 
+    COUNT(state) AS NumberOfaddress_book
+FROM
+    address_book;
    
     
