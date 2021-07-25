e2048a50e720c7eb36054cab39eafbf5430bb4d4
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-07-25 21:30:05 +0530

    adding the editing contacts part to the query

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -7,7 +7,6 @@ use address_book_service;
 
 #uc2
 #Ability to create a Address Book Table with first andlast names, address, city,state, zip, phone numberand email as its attributes.
-#Query:-
 create table address_book(
 	firstName varchar(30)  not null ,
     lastName varchar(30)  not null ,
@@ -15,7 +14,16 @@ create table address_book(
     city varchar(20)  not null,
 	state varchar(20)  not null,
 	zip  varchar(7) not null,
-	phoneNo varchar(7) not null,
-    email varchar(7) not null,
+	phoneNo varchar(10) not null,
+    email varchar(30) not null,
 	primary key(firstName)
 );
+#uc3
+#Ability to insert new Contacts to Address Book
+insert  into address_book(firstName,lastname,address,city,state,zip,phoneNo,email) values
+ ('Sunu','Nandakumar','Chanod','Vapi','Gujarat','396191','8153857156','as@gmail.com'),
+ ('akhil','vinod','Chanod Road','Vapi','Gujarat','396191','1234567123','as@gmail.com'),
+ ('bhavan','uthaman',' gunjan','Vapi','Gujarat','396191','8745961158','sa@gmail.com'),
+ ('achu','suraj','somewhere','valsad','Gujarat','396191','1236954125','cs@gmail.com');
+ #display Contacts in address_book table
+ select * from address_book;
